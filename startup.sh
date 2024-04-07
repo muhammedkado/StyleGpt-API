b67562b9f1fa6d19dc57c962c1b41907eee5586f
muhammedkado <mehmetkado9@gmail.com>
2024-04-07 17:53:48 +0300

    edit nginx conficiretion setting

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
-cp /home/site/default /etc/nginx/sites-available/default
-service nginx reload
+#!/bin/bash
+
+sed -i 's|root /home/site/wwwroot/;|root /home/site/wwwroot/public;|' /etc/nginx/sites-available/default
+sed -i '/location \/ {/ {n;s/.*/try_files $uri $uri\/ \/index.php?$args;/}' /etc/nginx/sites-available/default
+
+service nginx restart
